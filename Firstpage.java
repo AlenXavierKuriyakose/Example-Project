758f8b0e8d6ff65192dff7ed5aab43f238def4ce
Alen <alenkuriyakose07@gmail.com>
2025-07-30 00:24:11 +0530

    Java code added successfully

--- Firstpage.java
+++ Firstpage.java
@@ -1,5 +1,7 @@
+package Exampleproject;
+
 public class Firstpage {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, welcome to Firstpage!");
     }
 }
