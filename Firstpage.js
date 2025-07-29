dfc554718a015a87b9f6d7306f37433fbc7678c3
Alen <alenkuriyakose07@gmail.com>
2025-07-29 23:53:47 +0530

    js file successfully updated

---
+++ Firstpage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Firstpage() {
+    return (
+        <div>
+            <h1>Welcome to the First Page</h1>
+            <p>This is your starting point for the Exampleproject.</p>
+        </div>
+    );
+}
+
